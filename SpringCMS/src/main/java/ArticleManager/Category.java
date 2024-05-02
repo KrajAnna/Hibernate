7aec0f21283757665a9a09d7dfa90c82212cc9cd
KrajAnna <krajanna2505@gmail.com>
2024-05-02 20:57:50 +0200

    zad 1

---
+++ SpringCMS/src/main/java/ArticleManager/Category.java
@@ -0,0 +1,13 @@
+package ArticleManager;
+
+import javax.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+}
