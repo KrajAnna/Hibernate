82c7dbf4436843896025e59b0bd8deffd06515dd
KrajAnna <krajanna2505@gmail.com>
2024-05-11 12:21:32 +0200

    zad 5- wprowadzenie do walidacji

--- Spring01hibernate/src/main/java/pl/coderslab/entity/Publisher.java
+++ Spring01hibernate/src/main/java/pl/coderslab/entity/Publisher.java
@@ -1,8 +1,11 @@
 package pl.coderslab.entity;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.pl.NIP;
+import org.hibernate.validator.constraints.pl.REGON;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "publishers")
@@ -12,5 +15,10 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String name;
+    @NIP
+    private String nip;
+    @REGON
+    private String regon;
 }
